d41b6df2ae32d10c2bd99ba8edbce8224f0690cb
Chaiwat Suttipongsakul <cwt@bashell.com>
2026-01-15 11:12:26 +0700

    Update test facility and add ruff as a linter

--- scripts/code-format.sh
+++ scripts/code-format.sh
@@ -2,3 +2,7 @@
 
 poetry run black -t py312 -l 80 aiosyslogd/*.py
 poetry run black -t py312 -l 80 aiosyslogd/*/*.py
+poetry run black -t py312 -l 80 tests/*.py
+
+# Remove trailing whitespace in all .py files
+find . -name "*.py" -exec sed -i 's/[[:space:]]*$//' {} \;
